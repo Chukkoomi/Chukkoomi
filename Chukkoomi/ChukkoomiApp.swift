dcef7891f5669cade781ec5cf9d3106b38e0b1ef
Sunghun Park <dfstem9@gmail.com>
2025-11-04 23:00:10 +0900

    프로젝트 생성

---
+++ Chukkoomi/ChukkoomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChukkoomiApp.swift
+//  Chukkoomi
+//
+//  Created by 박성훈 on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ChukkoomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
