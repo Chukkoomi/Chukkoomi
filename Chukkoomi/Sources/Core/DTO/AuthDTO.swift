7fcd4cfad298b3d157d4f1b5a9a70cb76c445ce4
JIMIN-iOSDev <tig05015@naver.com>
2025-11-06 20:18:27 +0900

    Auth - DTO, Entity

---
+++ Chukkoomi/Sources/Core/DTO/AuthDTO.swift
@@ -0,0 +1,28 @@
+//
+//  AuthDTO.swift
+//  Chukkoomi
+//
+//  Created by 서지민 on 11/6/25.
+//
+
+import Foundation
+
+// MARK: - Refresh Token Response
+struct RefreshTokenResponseDTO: Decodable {
+    let accessToken: String
+    let refreshToken: String
+}
+
+extension RefreshTokenResponseDTO {
+    var toDomain: AuthToken {
+        return AuthToken(
+            accessToken: accessToken,
+            refreshToken: refreshToken
+        )
+    }
+}
+
+// MARK: - Auth Error Response
+struct AuthErrorResponseDTO: Decodable {
+    let message: String
+}
