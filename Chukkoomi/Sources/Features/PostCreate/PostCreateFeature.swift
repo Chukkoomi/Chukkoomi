dd034e2bfd2f040998e8570ccd2b0da19a1e6f9b
Sunghun Park <dfstem9@gmail.com>
2025-11-16 03:42:55 +0900

    PostCreateView, PostCreateFeature 생성

---
+++ Chukkoomi/Sources/Features/PostCreate/PostCreateFeature.swift
@@ -0,0 +1,8 @@
+//
+//  PostCreateFeature.swift
+//  Chukkoomi
+//
+//  Created by 박성훈 on 11/16/25.
+//
+
+import Foundation
---
+++ Chukkoomi/Sources/Features/PostCreate/PostCreateView.swift
@@ -0,0 +1,34 @@
+//
+//  PostCreateView.swift
+//  Chukkoomi
+//
+//  Created by 박성훈 on 11/16/25.
+//
+
+import SwiftUI
+
+struct PostCreateView: View {
+    
+    var body: some View {
+        // 버튼 누르면 갤러리 선택
+        
+        // 카테고리 선택
+        
+        // 해시태그
+        
+        // 글쓰기
+        
+        
+        // 업로드 버튼
+        FillButton(title: "업로드") {
+            // 게시글 업로드
+        }
+        
+    }
+}
+
+#Preview {
+    PostCreateView()
+}
+
+
